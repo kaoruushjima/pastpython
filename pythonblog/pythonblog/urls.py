29b58228213c354b0e7936f2af23326f20048ddb
ArsenalOkc <kaoruushjima@gmail.com>
2020-05-15 00:02:17 +0900

    add room controller : get room_id from url and returns text with room_id

--- pythonblog/pythonblog/urls.py
+++ pythonblog/pythonblog/urls.py
@@ -25,11 +25,15 @@ from django.http.response import HttpResponse
 
 
 def home(request):
-    return HttpResponse (' HELLO WORLD')
+    return HttpResponse ('HELLO WORLD')
 
+def room(request, room_id):
+    return HttpResponse("this is a room detail" + room_id)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('', home)
+    path('', home),
+
+    path('rooms/<room_id>', room)
 ]
