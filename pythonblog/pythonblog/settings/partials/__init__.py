30e2202b72c889b947e61654719cdbe6542776ea
ArsenalOkc <kaoruushjima@gmail.com>
2020-05-17 15:08:53 +0900

    refactor settings : seperate development settings from production settings

---
+++ pythonblog/pythonblog/settings/partials/__init__.py
@@ -0,0 +1,5 @@
+from .base import *
+from .static import *
+from .database import *
+from .Internationalization import *
+from .auth import *
---
+++ pythonblog/pythonblog/settings/partials/static.py
@@ -0,0 +1,8 @@
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+STATIC_URL = '/static/'
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
