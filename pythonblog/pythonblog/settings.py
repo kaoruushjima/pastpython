f93257bffe0fbcde78407de8d0bfbe90adc9948a
ArsenalOkc <kaoruushjima@gmail.com>
2020-05-17 14:23:29 +0900

    add django-debug-toolbar

--- pythonblog/pythonblog/settings.py
+++ pythonblog/pythonblog/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'debug_toolbar',
 
     'pythonblog', #장고 텝플릿을 이용할 준비 , application  이름
 ]
@@ -49,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'pythonblog.urls'
