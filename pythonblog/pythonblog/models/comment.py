2b37ead7cd9ef95302084d681f7f6abff6e36b9a
ArsenalOkc <kaoruushjima@gmail.com>
2020-05-20 23:11:07 +0900

    update posts list, detail view: add comments

--- pythonblog/pythonblog/models/comment.py
+++ pythonblog/pythonblog/models/comment.py
@@ -4,6 +4,7 @@ from django.db import models
 class Comment(models.Model):
 
     post = models.ForeignKey("Post", on_delete=models.CASCADE)
+    # 같은 앱(appolication)이라면, from .post import Post 라고 부를 필요 없이, "Post"라고 기재를 해도 되며, 더 좋은 코드이다.
 
     content = models.TextField()
 
