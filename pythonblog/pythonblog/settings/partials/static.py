a16fe99fda46f32c18c81f1b52b20d7acacef9c2
ArsenalOkc <kaoruushjima@gmail.com>
2020-05-22 00:59:43 +0900

    update media settings: add MEDIA_ROOT

--- pythonblog/pythonblog/settings/partials/static.py
+++ pythonblog/pythonblog/settings/partials/static.py
@@ -1,3 +1,8 @@
+import os
+
+from .base import PROJECT_ROOT_DIR
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
@@ -6,3 +11,10 @@ STATIC_URL = '/static/'
 INTERNAL_IPS = [
     '127.0.0.1',
 ]
+
+# Media Files
+MEDIA_ROOT = os.path.join(
+    PROJECT_ROOT_DIR,
+    "dist",
+    "media",
+)
