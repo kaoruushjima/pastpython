37082f29b0dd165ddd7ac88617c9c610b8bf6b08
ArsenalOkc <kaoruushjima@gmail.com>
2020-05-23 00:04:37 +0900

    init auth views: login, signup

--- pythonblog/pythonblog/views/auth/login.py
+++ pythonblog/pythonblog/views/auth/login.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login as auth_login
 
 
 def login(request):
+    if (request.method == "POST"):
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            auth_login(request, user)
+            return redirect(reverse("home"))
+
+        return redirect(reverse("auth:login"))
+
     return render(
         request,
         "auth/login.html",
--- pythonblog/pythonblog/views/auth/signup.py
+++ pythonblog/pythonblog/views/auth/signup.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib.auth.models import User
 
 
 def signup(request):
+    if (request.method == "POST"):
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        email = request.POST.get("email")
+
+        user = User.objects.create_user(
+            username=username,
+            password=password,
+            email=email,
+        )
+
+        return redirect(reverse("auth:login"))
+
     return render(
         request,
         "auth/signup.html",
