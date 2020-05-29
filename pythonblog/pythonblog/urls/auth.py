01fd4619b0f1b6c6a661c0b728ba2fcb254a1cbd
ArsenalOkc <kaoruushjima@gmail.com>
2020-05-29 22:09:27 +0900

    refactor LoginView, LogoutView, SignupView

--- pythonblog/pythonblog/urls/auth.py
+++ pythonblog/pythonblog/urls/auth.py
@@ -5,8 +5,8 @@ from pythonblog.views.auth import *
 
 app_name = 'auth'
 urlpatterns = [
-    path('login/', login, name="login"),
-    path('signup/', signup, name="signup"),
-    path('logout/', logout, name="logout"),
+    path('login/', LoginView.as_view(), name="login"),
+    path('signup/', SignupView.as_view(), name="signup"),
+    path('logout/', LogoutView.as_view(), name="logout"),
     path('my/page/', mypage, name="mypage"),
 ]
--- pythonblog/pythonblog/views/auth/logout.py
+++ pythonblog/pythonblog/views/auth/logout.py
@@ -1,8 +1,10 @@
 from django.shortcuts import redirect
 from django.urls import reverse
-from django.contrib.auth import logout as auth_logout
+from django.contrib.auth import logout
+from django.views.generic import View
 
 
-def logout(request):
-    auth_logout(request)
-    return redirect(reverse("home"))
+class LogoutView(View):
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        return redirect(reverse("home"))
