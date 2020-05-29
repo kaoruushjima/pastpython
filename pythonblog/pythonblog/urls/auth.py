3e64e419f70544bcbb2e8055e8039884cc1dc409
ArsenalOkc <kaoruushjima@gmail.com>
2020-05-29 22:19:03 +0900

    update MypageView: change to CBV using LoginRequiredMixin

--- pythonblog/pythonblog/urls/auth.py
+++ pythonblog/pythonblog/urls/auth.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name="login"),
     path('signup/', SignupView.as_view(), name="signup"),
     path('logout/', LogoutView.as_view(), name="logout"),
-    path('my/page/', mypage, name="mypage"),
+    path('my/page/', MypageView.as_view(), name="mypage"),
 ]
