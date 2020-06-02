394d9e01377ca4dd6610a2710f9416ba704fa0f8
ArsenalOkc <kaoruushjima@gmail.com>
2020-06-02 21:14:43 +0900

    init post_save_user signal: create user_profile on post save user

---
+++ pythonblog/users/models/user_profile.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models.signals import post_save
+
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    phonenumber = models.CharField(
+        max_length=16,
+        blank=True,
+        null=True,
+    )
+
+    address = models.CharField(
+        max_length=64,
+        blank=True,
+        null=True,
+    )
+
+
+def post_save_user(sender, instance, created, **kwargs):
+    if created:
+        user_profile = UserProfile.objects.create(
+            user=instance
+        )
+
+
+post_save.connect(post_save_user, sender=User)
